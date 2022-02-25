2bc02912e4a4df303c135618d6ff5584645cf2ff
Николай Бутусов <99186896+Bkolanyc@users.noreply.github.com>
2022-02-25 15:21:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,8 @@
 
 
 bool checkPrime(uint64_t value) {
-
+if (value == 2) return true;
+if (value % 2 == 0) return false;
 for (i = 2; i <= sqrt(value); i++) {
     if (n % i == 0) {
        return false
