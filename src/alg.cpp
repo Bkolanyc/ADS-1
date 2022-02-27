c2ec0244dd7a2f8dae6ce094273fec90095ead2b
Николай Бутусов <99186896+Bkolanyc@users.noreply.github.com>
2022-02-27 12:28:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,9 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
+#include <iostream>
 #include "alg.h"
-
+#include <ctime>
+using namespace std;
 
 bool checkPrime(uint64_t value) {
 if (value == 2) return true;
@@ -30,9 +32,27 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  if (value == 2)
+    return 3;
+  if (checkPrime (value) == 1)
+    value += 2;
+  if (value % 2 == 0)
+    value += 1;
+  while (checkPrime (value) == 0)
+    value += 2;
+  return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  uint64_t prime = 3;
+  uint64_t sum = 2;
+  if (hbound <= 2)
+    return 0;
+  while (prime < hbound)
+    {
+      if (checkPrime (prime) == 1)
+	sum += prime;
+      prime += 2;
+    }
+  return sum;
 }
