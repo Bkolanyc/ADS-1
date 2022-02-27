50b907e01b61a9e2c712c3979cbf0ecc98ff33fe
Николай Бутусов <99186896+Bkolanyc@users.noreply.github.com>
2022-02-27 12:35:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -15,49 +15,13 @@ return false
 return true;
 }
 uint64_t nPrime(uint64_t n) {
-uint64_t prime = 1;
-if (n == 1) return 2;
-for (int i = 1; i < n; i++) {
-prime += 2;
-while (checkPrime(prime) == 0) {
-prime += 2;
+// вставьте код функции
 }
-}
-return prime;
-}
+
 uint64_t nextPrime(uint64_t value) {
-if (value == 2)
-return 3;
-if (checkPrime (value) == 1)
-value += 2;
-if (value % 2 == 0)
-value += 1;
-while (checkPrime (value) == 0)
-value += 2;
-return value;
+// вставьте код функции
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-uint64_t prime = 3;
-uint64_t sum = 2;
-if (hbound <= 2)
-return 0;
-while (prime < hbound)
-{
-if (checkPrime (prime) == 1)
-sum += prime;
-prime += 2;
-}
-return sum;
-}
-int main() {
-unsigned int start_time = clock();
-cout « nPrime(500) « endl;
-cout « nextPrime(1831) « endl;
-cout « checkPrime(3559) « endl;
-cout « sumPrime(2000000) « endl;
-cout « sumPrime(1) « endl;
-unsigned int end_time = clock(); // конечное время
-unsigned int search_time = end_time - start_time; // искомое время
-cout « "runtime = " « search_time / 1000.0 « " seconds" « endl « endl;
+// вставьте код функции
 }
